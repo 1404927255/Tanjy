f024959420632f0e57f8b182f02275657db86b5c
1404927255 <115683268+1404927255@users.noreply.github.com>
2022-10-13 12:50:27 +0800

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>;
+int main()
+{
+	printf("孤独的世界\n");
+	printf("总会有一个人陪伴\n");
+	printf("那个人会是谁\n");
+	printf("会是你吗\n");
+	printf("i love you\n");
+	printf("i love you\n");
+	return 0;
+}
